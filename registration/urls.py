e74cb03a4857801e39788aa5dd2de6e61e77456e
balntvdd <bonatividadjr.student@ua.edu.ph>
2025-10-08 12:08:51 +0800

    first commit

--- registration/urls.py
+++ registration/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('api/register/', views.register_user, name='register_api'),
+    path('api/users/', views.list_user, name='list_user'),
+    path('api/users/<int:pk>/', views.user_detail, name='user_detail'),
 ]
-
-
+    
--- registration/views.py
+++ registration/views.py
@@ -16,3 +16,31 @@ def  register_user(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+def list_user(request):
+    users = UserRegistration.objects.all()
+    serializer = RegistrationSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = UserRegistration.objects.get(pk=pk)
+    except UserRegistration.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = RegistrationSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = RegistrationSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+
+    elif request.method == 'DELETE':
+        user.delete()               
+        return Response(status=status.HTTP_204_NO_CONTENT)
